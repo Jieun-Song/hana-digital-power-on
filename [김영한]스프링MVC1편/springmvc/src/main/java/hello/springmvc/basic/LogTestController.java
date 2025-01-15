3073b90047b6a6b14013e9aadf969d1f5d46ed41
jieun song <0scarl1216@mju.ac.kr>
2025-01-15 15:20:46 +0900

    [20250115]섹션7 HTTP 요청 학습

--- [김영한]스프링MVC1편/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ [김영한]스프링MVC1편/springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,20 +1,25 @@
 package hello.springmvc.basic;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class LogTestController {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
+//    private final Logger log = LoggerFactory.getLogger(getClass());
+// Slf4j 어노테이션을 사용하면 위 코드를 생략할 수 있다.
     @RequestMapping("/log-test")
     public String logTest() {
         String name = "Spring";
 
-        System.out.println("name = "+name );
+        System.out.println("name = " + name );
+
+        log.trace("trace log = {}", name);
+        log.debug("debug log = {}", name);
         log.info("info log = {}", name);
+        log.warn("warn log = {}", name);
+        log.error("error log = {}", name);
 
         return "ok";
     }
